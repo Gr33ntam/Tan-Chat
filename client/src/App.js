49d13940b5c92a590fef12c109ddef832e19e389
Sean McClure <seanmcclure2000@yahoo.co.uk>
2026-01-20 09:31:34 +0000

    Add Leaderboard route to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Chat from './Chat';
 import Admin from './Admin';
 import Account from './Account';
+import Leaderboard from './Leaderboard';
 
 function App() {
   const [username, setUsername] = useState(localStorage.getItem('username') || '');
@@ -19,6 +20,7 @@ function App() {
         <Route path="/" element={<Chat onUsernameSet={setUsername} />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/account" element={<Account username={username} onLogout={handleLogout} />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
       </Routes>
     </Router>
   );
